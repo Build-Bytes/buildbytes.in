b0f0b443e5570c8a880614fad900b44c439e2f20
Prathamesh Khade <pkhade2865@gmail.com>
2024-08-07 22:06:12 +0530

    rm: framer motion animation

--- src/frontend/src/components/Hero.jsx
+++ src/frontend/src/components/Hero.jsx
@@ -1,40 +1,25 @@
 import React, { useRef, useState } from 'react'
-// import Cursor from './Cursor';
-import { motion } from 'framer-motion';
+
+// hooks
 import useMousePosition from '../hooks/useMousePosition';
 
 // image 
 import blackCircle from '../assets/black-circle.svg'
 
 export default function Hero() {
-    // const [cursorSize, setCursorSize] = useState(25);
-    const {x, y} = useMousePosition();
 
-    const varients = {
-        initial1: {
-            opacity: 0,
-            x: -500,
-        },
-        initial2: {
-            opacity: 0,
-            x: 500,
-        },
-        final: {
-            opacity: 1,
-            x: 0
-        }
-    }
+    const {x, y} = useMousePosition();
 
     return (
         <section className="w-full h-screen flex justify-center flex-col overflow-hidden">
-            <motion.div className='font-extrabold text-center' variants={varients} initial='initial1' animate='final' transition={{ delay: 0.8, type: 'spring', stiffness: 300 }}>
+            <div className='font-extrabold text-center'>
                 <h1 className='text-6xl md:text-8xl lg:text-10xl'>Welcome to</h1>
                 <h1 className='text-6xl md:text-8xl lg:text-10xl'>BuildBytes.</h1>
-            </motion.div>
-            <motion.div className='text-myOrange font-extrabold text-center' variants={varients} initial='initial2' animate='final' transition={{ delay: 0.8, type: 'spring', stiffness: 300 }}>
+            </div>
+            <div className='text-myOrange font-extrabold text-center'>
                 <h1 className='text-6xl md:text-8xl lg:text-10xl'>We Build,</h1>
                 <h1 className='text-6xl md:text-8xl lg:text-10xl'>Your Future.</h1>
-            </motion.div>
+            </div>
             <div 
                 className='max-lg:hidden text-black absolute w-full h-screen flex justify-center flex-col font-extrabold text-center text-8xl'
                 style={{
